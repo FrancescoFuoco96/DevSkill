4ddfbe1ba61ae7b315a78ea78601244e1cf99bf6
FrancescoFuoco96 <102958337+FrancescoFuoco96@users.noreply.github.com>
2022-04-21 16:13:36 +0200

    Positivo o Negativo con Funzione

---
+++ Positivo o Negativo Funzione.ts
@@ -0,0 +1,13 @@
+function positivonegativo() {
+    if (numero>=0) {
+            alert("il numero inserito è: POSITIVO");
+     }
+    else {
+            alert("Il numero inserito è: NEGATIVO");
+}
+
+}
+
+let numero:number= prompt("Inserisci un numero qualsiasi");
+
+positivonegativo();
