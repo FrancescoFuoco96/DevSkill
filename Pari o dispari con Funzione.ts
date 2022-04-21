fa83ad6012e9dd0be2d603e7e2d426ffe032804a
FrancescoFuoco96 <102958337+FrancescoFuoco96@users.noreply.github.com>
2022-04-21 16:13:01 +0200

    Pari o dispari con Funzione

---
+++ Pari o dispari con Funzione.ts
@@ -0,0 +1,13 @@
+function paridispari() {
+    
+    if (numero%2 ==0) {
+        alert("Il numero inserito è PARI");
+    }
+    else {
+        alert("Il numero inserito è DISPARI");
+    }
+}
+
+let numero:number= prompt("Inserisci un numero qualsiasi");
+
+paridispari();
